1fbc72b9d05bd05f1d4314b8fd4bddbccd7c3ed8
krish962 <balakrishnan.logicfocus@gmail.com>
2023-10-11 17:33:40 +0530

    Initial Commit

---
+++ RentyVilla/RentyVillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RentyVillaApp.swift
+//  RentyVilla
+//
+//  Created by Logicfocus Techonology Solution on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RentyVillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
